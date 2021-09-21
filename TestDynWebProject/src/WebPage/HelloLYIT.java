03c073dba3f182a37e6ec7fcea23da693f3349a8
mariagriffin <maria.griffin@lyit.ie>
2021-09-21 20:13:36 +0100

    Adding Hello world

---
+++ TestDynWebProject/src/WebPage/HelloLYIT.java
@@ -0,0 +1,6 @@
+public class HelloLYIT {
+    public static void main(String[] args) {
+        // Prints "Hello, LYIT from Ruth" as output
+        System.out.println("Hello, LYIT from Ruth");
+    }
+}
